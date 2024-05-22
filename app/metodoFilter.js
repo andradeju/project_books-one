b92252efb5acacdb0581c926e4a36b2d47a327c6
Juliana Andrade <julliana.andrade08@gmail.com>
2024-05-21 21:42:40 -0300

    :sparkles: feat: Add metodo que mostra livros disponiveis/indisponiveis na tela

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,6 +6,26 @@ btnFiltros.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros(){
   const elementoBtn = document.getElementById(this.id)
   const categoria = elementoBtn.value
-  let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+  let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
   exibirLivrosNaTela(livrosFiltrados)
+  if(categoria == 'disponivel') {
+    const valorTotal = calcularTotalLivrosDisponiveis(livrosFiltrados)
+    exibirValorTotalLivrosDisponiveisNaTela(valorTotal)
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalLivrosDisponiveisNaTela(valorTotal) {
+  elementoValorTotalLivrosDisponiveis.innerHTML = `
+  <div class="livros__disponiveis">
+  <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+</div>
+  `
 }
